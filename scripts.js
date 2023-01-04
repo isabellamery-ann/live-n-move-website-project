f4d798d00b81263ea6872a3cc0e2829583373e9d
isabellamery-ann <117369679+isabellamery-ann@users.noreply.github.com>
2023-01-04 14:23:32 +0100

    site-settings.yml

--- scripts.js
+++ scripts.js
@@ -2,12 +2,28 @@
 $( document ).ready(function() {
     
     
-    $('.hamburger-button').click(function(){
-        $('.menu').fadeToggle(100);
-        $(this).toggleClass('active');
+    $(".hamburger-button").click(function(){
+      
+      $(this).toggleClass('active');  
+      $('.menu').fadeToggle();
+        
         
       });
 
       // Animate on scroll - activate
-      AOS.init();
+      AOS.init({
+        duration: 500 //values from 0 to 3000, with step 50ms
+      });
+});
+
+
+
+lightGallery(document.getElementById('lightgallery'), {
+  speed: 500,
+  download: false
+});
+
+lightGallery(document.getElementById('lightgallery-2'), {
+  speed: 500,
+  download: false
 });
